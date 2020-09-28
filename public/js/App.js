ca3ec4117c5f2a3826b81e8f28a627f424f3f34c
Sanni-park <Sanni-park@users.noreply.github.com>
2020-09-28 21:28:44 +0900

    Save changes

--- public/js/App.js
+++ public/js/App.js
@@ -16,7 +16,7 @@ let App = {
      * Do not use in production.
      */
     loadTemplates: function() {
-        let templateArr = ["center", "footer", "header", "left", "right"];
+        let templateArr = ["center", "footer", "header"];
         let htmlCollection = document.getElementsByClassName("grid");
         let gridArray = [].slice.call(htmlCollection);
         document.querySelectorAll("*").forEach(function(node) {
@@ -28,3 +28,11 @@ let App = {
 
 };
 
+function dropDownMenu() {
+  var x = document.getElementById("top-nav");
+  if (x.className === "contents-top-navigation") {
+    x.className += " responsive";
+  } else {
+    x.className = "contents-top-navigation";
+  }
+}
